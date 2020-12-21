57665ea6eff8f21afbba44a636be6aed8de8632e
Eric Schmidt <em.schmidt78@gmail.com>
2020-12-20 19:27:59 -0800

    feat: adds GLFW and GLUT to game

--- app/main.cpp
+++ app/main.cpp
@@ -5,20 +5,69 @@ Version: 2020-11-29
 
 Entry point for the game.
 */
+#include <GLUT/glut.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 #include <string>
+#include <stdlib.h>
+#include <stdio.h>
 #include "Character.h"
 
 using namespace std;
 
-int main(int argc, char* argv[])
+
+int main(int argc, char **argv)
 {
-    cout << "Running app executable ... " << endl;
-    cout << "Creating base character class ..." << endl;
 
-    Character dude("Dude", 6, 1);
+  Character person;
+  string name = person.GetName();
 
-    cout << "Executable done." << endl;
+  GLFWwindow* window;
+  if (!glfwInit())
+  {
+    fprintf( stderr, "Failed to initialize GLFW\n" );
+    getchar();
+    exit(EXIT_FAILURE);
+  }
+  window = glfwCreateWindow(640, 480, name.c_str(), NULL, NULL);
+  if (!window)
+  {
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+  glfwMakeContextCurrent(window);
+  while (!glfwWindowShouldClose(window))
+  {
+    float ratio;
+    int width, height;
 
-    return 0;
+    glfwGetFramebufferSize(window, &width, &height);
+    ratio = (float) width / (float) height;
+
+    glViewport(0, 0, width, height);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glRotatef((float)glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.f, 0.f, 0.f);
+    glVertex3f(-0.6f, -0.4f, 0.f);
+    glColor3f(0.f, 1.f, 0.f);
+    glVertex3f(0.6f, -0.4f, 0.f);
+    glColor3f(0.f, 0.f, 1.f);
+    glVertex3f(0.f, 0.6f, 0.f);
+    glEnd();
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  exit(EXIT_SUCCESS);
 }
